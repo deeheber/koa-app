aba313f5a89e76bacb6a7e64226290b3b95d6187
Danielle H <daheber@gmail.com>
2016-08-04 12:10:38 -0700

    first model and starting routing

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const port = process.env.PORT || 3000;
 require('./lib/setup-mongoose');
 const koa = require('koa');
-const app = koa();
+const app = module.exports = koa();
 
+var router = require('koa-router')();
+
+
+// logger
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// routes
+router.get('/stuff', function *() {
+  this.body = 'welcome to the stuff page';
+});
+// response
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.use(function *(){
   this.body = 'Hello World';
-});
-
-app.on('error', function(err){
-  console.error('server error', err);
 });
 
 app.listen(port, ()=>{
