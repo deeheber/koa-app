7924bc4ad3badeedf00313daf3f71a590597983c
Danielle H <daheber@gmail.com>
2016-08-04 10:30:38 -0700

    what I have so far

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ require('./lib/setup-mongoose');
 const koa = require('koa');
 const app = koa();
 
+
 app.use(function *(){
   this.body = 'Hello World';
+});
+
+app.on('error', function(err){
+  console.error('server error', err);
 });
 
 app.listen(port, ()=>{
