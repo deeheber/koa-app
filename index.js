4a9f7f518cadb8a223c5f6a42f11b8bffbcebaf2
Danielle H <daheber@gmail.com>
2016-08-04 13:48:50 -0700

    routes seperated out

--- index.js
+++ index.js
@@ -2,31 +2,24 @@ const port = process.env.PORT || 3000;
 require('./lib/setup-mongoose');
 const koa = require('koa');
 const app = module.exports = koa();
-
-var router = require('koa-router')();
-
+const dogRouter = require('./lib/routes/dogs');
 
 // logger
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
+// app.use(function *(next){
+//   var start = new Date;
+//   yield next;
+//   var ms = new Date - start;
+//   console.log('%s %s - %s', this.method, this.url, ms);
+// });
 
-// routes
-router.get('/stuff', function *() {
-  this.body = 'welcome to the stuff page';
-});
-// response
+//Hello world
+// app.use(function *(){
+//   this.body = 'Hello World';
+// });
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-app.use(function *(){
-  this.body = 'Hello World';
-});
+  .use(dogRouter.routes())
+  .use(dogRouter.allowedMethods());
 
 app.listen(port, ()=>{
   console.log(`Server started on port ${port}`);
