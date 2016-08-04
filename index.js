bb8018b3d94caa7596b31c90eecffb28fdb86ccc
Danielle H <daheber@gmail.com>
2016-08-04 16:03:40 -0700

    error message for get by id

--- index.js
+++ index.js
@@ -5,33 +5,16 @@ const koa = require('koa');
 const app = module.exports = koa();
 const router = require('./lib/routes/dogs');
 
-// logger
-// app.use(function *(next){
-//   var start = new Date;
-//   yield next;
-//   var ms = new Date - start;
-//   console.log('%s %s - %s', this.method, this.url, ms);
-// });
-
-//Hello world
-// app.use(function *(){
-//   this.body = 'Hello World';
-// });
 
 app
-  .use(function *(next) {
-    try {
-      yield next;
-    } catch (err) {
-      this.status = err.status || 500;
-      this.body = err.message;
-      this.app.emit('error', err, this);
+  .use(parser({
+    onerror: function (err, ctx) {
+      ctx.throw('Make sure you entered valid JSON and included all required fields.', 400);
     }
-  })
-  .use(parser())
+  }))
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
 
-app.listen(port, ()=>{
-  console.log(`Server started on port ${port}`);
-});
+  .listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+  });
