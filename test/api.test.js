8c193c76ddac90f5627dd0af555a30b6be47a505
Danielle H <daheber@gmail.com>
2016-08-05 13:15:42 -0700

    test plan done in comments

---
+++ test/api.test.js
@@ -0,0 +1,16 @@
+// before block Clear the DB
+
+// Error on invlid URL path
+
+// POST -- creates a dog (or two)
+
+// GET -- returns all dogs
+
+// PUT -- updates a dog
+
+// GET by ID -- returns a single dog
+// Error on invalid ID
+
+// DELETE -- removes a dog
+
+// after block close the db connection
---
+++ test/dog.test.js
@@ -0,0 +1,5 @@
+//creates a dog
+
+// requires a name
+
+//max age is 25
