7e1ce4333467681054ef69ba10ded2c64f85705f
Danielle H <daheber@gmail.com>
2016-08-05 09:13:25 -0700

    seperated out index from app

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
+const app = require('./lib/app.js');
 const port = process.env.PORT || 3000;
-require('./lib/setup-mongoose');
-const parser = require('koa-bodyparser');
-const koa = require('koa');
-const app = module.exports = koa();
-const router = require('./lib/routes/dogs');
-
 
 app
-  .use(parser({
-    onerror: function (err, ctx) {
-      ctx.throw('Make sure you entered valid JSON and included all required fields.', 400);
-    }
-  }))
-  .use(router.routes())
-  .use(router.allowedMethods())
-
   .listen(port, ()=>{
     console.log(`Server started on port ${port}`);
   });
