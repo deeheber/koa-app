85197c02a2adb0f6ea6afe7d0bd897781236affb
Danielle H <daheber@gmail.com>
2016-08-14 12:13:59 -0700

    unit testing complete

--- test/dog.test.js
+++ test/dog.test.js
@@ -1,5 +1,28 @@
-//creates a dog
+const Dog = require('../lib/models/dog');
+const assert = require('chai').assert;
 
-// requires a name
+describe('dog model unit test', ()=>{
 
-//max age is 25
+  it('creates a dog', done=>{
+    const fido = new Dog({name: 'Fido', breed: 'Pit bull', age: 4});
+    assert.equal(fido.name, 'Fido');
+    assert.equal(fido.breed, 'Pit bull');
+    done();
+  });
+
+  it('requires a name', done=>{
+    const noname = new Dog();
+    noname.validate(err=>{
+      if(!err) done('error not thrown for no name');
+      else done();
+    });
+  });
+
+  it('error when age is over 25', done=>{
+    const oldDog = new Dog({name: 'barnaby', age: 50});
+    oldDog.validate(err=>{
+      if(!err) done('error not thrown for age over 25');
+      else done();
+    });
+  });
+});
