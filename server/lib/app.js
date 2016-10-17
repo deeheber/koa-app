3c8b44d835ce5a664ec1300e9ba66f3395636192
Danielle H <daheber@gmail.com>
2016-10-17 14:10:24 -0700

    koa-cors added

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,13 +1,15 @@
 require('./setup-mongoose');
 const parser = require('koa-bodyparser');
 const serve = require('koa-static');
-const logger = require('koa-logger')
+const logger = require('koa-logger');
+const cors = require('koa-cors');
 const koa = require('koa');
 const app = module.exports = koa();
 const router = require('./routes/dogs');
 
 app
   .use(logger())
+  .use(cors())
   .use(serve(__dirname + '/public'))
   .use(parser({
     onerror: function (err, ctx) {
