5804e844c510d0126f35a68edfcec59eba6a1d3b
Danielle H <daheber@gmail.com>
2016-10-17 13:50:42 -0700

    serves static files through koa

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,11 +1,12 @@
 require('./setup-mongoose');
 const parser = require('koa-bodyparser');
+const serve = require('koa-static');
 const koa = require('koa');
 const app = module.exports = koa();
 const router = require('./routes/dogs');
 
-
 app
+  .use(serve(__dirname + '/public'))
   .use(parser({
     onerror: function (err, ctx) {
       ctx.throw('Make sure you entered valid JSON and included all required fields.', 400);
