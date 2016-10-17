134e9d559fbc3600f791320833504340264f8cd7
Danielle H <daheber@gmail.com>
2016-10-17 14:04:40 -0700

    logger added

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,11 +1,13 @@
 require('./setup-mongoose');
 const parser = require('koa-bodyparser');
 const serve = require('koa-static');
+const logger = require('koa-logger')
 const koa = require('koa');
 const app = module.exports = koa();
 const router = require('./routes/dogs');
 
 app
+  .use(logger())
   .use(serve(__dirname + '/public'))
   .use(parser({
     onerror: function (err, ctx) {
